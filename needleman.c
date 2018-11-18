758e99223ee05d6001272d4d8710ad40b13008c1
PS Narayanan <thepsnarayanan@gmail.com>
2018-11-18 14:34:16 +0530

    Bug fix

--- needleman.c
+++ needleman.c
@@ -11,7 +11,7 @@ void TraceBack(char[],char[]);
 int M[20][20];
 int GAP = -2;
 int MATCH = 5;
-int MISSMATCH = -1;
+int MISSMATCH = - 1;
 
 int main(){
 
@@ -32,12 +32,12 @@ void initialize(char seq1[],char seq2 []){
     int seq1len = strlen(seq1);
     int seq2len = strlen(seq2);
     M[0][0] = 0;
-    for (int i = 0; i < seq1len; i++){
+    for (int i = 0; i < seq1len +  1; i++){
 
         M[i][0] = i == 0 ? 0 : M[i-1][0] + (GAP); 
     }
 
-    for (int j = 0; j < seq2len; j++){
+    for (int j = 0; j < seq2len + 1; j++){
 
         M[0][j] = j == 0 ? 0 : M[0][j-1] + (GAP);
     }
@@ -47,21 +47,24 @@ void initialize(char seq1[],char seq2 []){
 void ScoreTable(char seq1[],char seq2 []){
     int seq1len = strlen(seq1);
     int seq2len = strlen(seq2);
-    for (int i = 1; i < seq1len; i++)
+    int a = -2;
+    for (int i = 1; i < seq1len + 1; i++)
     {
-        for (int j = 1; j < seq2len; j++)
+        for (int j = 1; j < seq2len + 1; j++)
         {
-            int scroeDiag = 0;
+            int scoreDiag = 0;
 
-            if (seq1[j - 1] == seq2[i - 1])
-                scroeDiag = M[i - 1][j - 1] + MATCH;
-            else
-                scroeDiag = M[i - 1][j - 1] + MISSMATCH;
+            if (seq1[j - 1] == seq2[i - 1]){
+                scoreDiag = M[i - 1][j - 1] + MATCH;
+            }
+            else{
+                scoreDiag = M[i - 1][j - 1] + MISSMATCH;
+            }
 
-            int scroeLeft = M[i][j - 1] - GAP;
-            int scroeUp = M[i - 1][j] - GAP;
+            int scoreLeft =(M[i][j - 1] + GAP);
+            int scoreUp = (M[i - 1][j] + GAP);
 
-            int maxScore = MAX(MAX(scroeDiag, scroeLeft), scroeUp);
+            int maxScore = MAX(MAX(scoreDiag, scoreLeft), scoreUp);
             M[i][j] = maxScore;
         }
     }
@@ -73,6 +76,8 @@ void TraceBack(char seq1[],char seq2 []){
     char AlignA,AlignB;
     int m = strlen(seq1) - 1;
     int n = strlen(seq2) - 1;
+    int seq1len = strlen(seq1);
+    int seq2len = strlen(seq2);
 
     while (m > 0 && n > 0){
         int score = 0;
@@ -83,31 +88,30 @@ void TraceBack(char seq1[],char seq2 []){
         else{
             score = MISSMATCH;
         }
-        if (m > 0 && n > 0 && M[m, n] == M[m - 1, n - 1] + score)
+        if (m > 0 && n > 0 && M[m][n] == M[m - 1][n - 1] + score)
         {
             AlignA = seq1[n - 1] + AlignA;
             AlignB = seq2[m - 1] + AlignB;
             m = m - 1;
             n = n - 1;
         }
-        else if (n > 0 && M[m, n] == M[m, n - 1] - 2)
+        else if (n > 0 && M[m][n] == M[m][n - 1] - 2)
         {
             AlignA = seq1[n - 1] + AlignA;
-            AlignB = "-" + AlignB;
+            AlignB = '-' + AlignB;
             n = n - 1;
         }
         else 
         {
-            AlignA = "-" + AlignA;
+            AlignA = '-' + AlignA;
             AlignB = seq2[m - 1] + AlignB;
             m = m - 1;
         }
     }
-    int seq1len = strlen(seq1);
-    int seq2len = strlen(seq2);
-    for (int i = 0; i < seq1len; i++){
+    
+    for (int i = 0; i < seq1len + 1; i++){
         printf("\n");
-        for (int j = 0; j < seq2len; j++){
+        for (int j = 0; j < seq2len + 1; j++){
             printf("%d\t",M[i][j]);
         }
 
